0cdf8a1361464c2dc60592c8952025164b66f0b1
Irfan Chairurrachman <irfanchairurrachman@gmail.com>
2021-02-21 21:45:21 +0700

    feat: add date and parent py (222)

---
+++ 2_PythonInteractOS/week2/file_date.py
@@ -0,0 +1,15 @@
+import os
+import datetime
+
+def file_date(filename):
+    # Create the file in the current directory
+    open(filename, "w")
+    timestamp = os.path.getmtime(filename)
+    # Convert the timestamp into a readable format, then into a string
+    date = str(datetime.datetime.fromtimestamp(timestamp))
+    # Return just the date portion 
+    # Hint: how many characters are in “yyyy-mm-dd”? 
+    return ("{:.10s}".format(date))
+    
+print(file_date("newfile.txt")) 
+# Should be today's date in the format of yyyy-mm-dd
