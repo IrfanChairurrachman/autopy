e9bad2e7a40b9007895ccc637bf9997b733d31de
Irfan Chairurrachman <irfanchairurrachman@gmail.com>
2021-02-26 21:54:39 +0700

    feat: add syslog and fix ticky.py (271)

--- 2_PythonInteractOS/week7/ticky_check.py
+++ 2_PythonInteractOS/week7/ticky_check.py
@@ -14,22 +14,25 @@ with open('syslog.log') as file:
     # Iterate each line in file
     for log in file.readlines():
         # search format with regex
-        result = re.search(r"ticky: ([INFO|ERROR]*): (['\w ]*) [\[[#\d]*\]?]? ?\(([.\w]*)\)$", log)
+        result = re.search(r"ticky: ([INFO|ERROR]*) (['\w ]*) [\[[#\d]*\]?]? ?\(([.\w]*)\)$", log)
 
+        if result is None:
+            continue
         # initiate user in dict if doesn't exist before
-        if result[3] not in per_user:
+        if result.group(3) not in per_user:
             # first list for INFO, second for ERROR
-            per_user[result[3]] = [0, 0]
+            per_user[result.group(3)] = [0, 0]
 
         # add INFO and ERROR
-        if result[1] == 'INFO':
-            per_user[result[3]][0] += 1
-        elif result[1] == 'ERROR':
-            per_user[result[3]][1] += 1
+        if result.group(1) == 'INFO':
+            per_user[result.group(3)][0] += 1
+        elif result.group(1) == 'ERROR':
+            per_user[result.group(3)][1] += 1
             # Add error to error dict
-            if result[2] not in error:
-                error[result[2]] = 0
-            error[result[2]] += 1
+            if result.group[2] not in error:
+                error[result.group[2]] = 0
+            error[result.group[2]] += 1
+        # print("{} {} {}".format(result[1], result[2], result[3]))
 
 # Sorted dictionary
 per_user = sorted(per_user.items())
