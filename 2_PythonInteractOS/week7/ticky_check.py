17e1471e6a9875876526a992f62cfbdfbc5894d1
Irfan Chairurrachman <irfanchairurrachman@gmail.com>
2021-02-26 14:29:35 +0700

    doc: add doc (271)

--- 2_PythonInteractOS/week7/ticky_check.py
+++ 2_PythonInteractOS/week7/ticky_check.py
@@ -5,20 +5,28 @@ import sys
 import re
 import operator
 
+# Create dictionary
 error = {}
 per_user = {}
 
+# Open file and close after do things
 with open('syslog.log') as file:
-    for log in file:
+    # Iterate each line in file
+    for log in file.readlines():
+        # search format with regex
         result = re.search(r"ticky: ([INFO|ERROR]*): (['\w ]*) [\[[#\d]*\]?]? ?\(([.\w]*)\)$", log)
 
+        # initiate user in dict if doesn't exist before
         if result[3] not in per_user:
+            # first list for INFO, second for ERROR
             per_user[result[3]] = [0, 0]
 
+        # add INFO and ERROR
         if result[1] == 'INFO':
             per_user[result[3]][0] += 1
         elif result[1] == 'ERROR':
             per_user[result[3]][1] += 1
+            # Add error to error dict
             if result[2] not in error:
                 error[result[2]] = 0
             error[result[2]] += 1
