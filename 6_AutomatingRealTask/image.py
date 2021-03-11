4a7e2cf7f2ba53aa9f6deb5f9a2de34d8ac61c61
Irfan Chairurrachman <irfanchairurrachman@gmail.com>
2021-03-11 21:14:15 +0700

    feat: add image.py (61)

---
+++ 6_AutomatingRealTask/image.py
@@ -0,0 +1,23 @@
+from PIL import Image
+import os
+
+def edit_image(source, dest, rotate=-90, x=128, y=128):
+    for image in os.listdir(source):
+        
+        f = "{}/{}".format(source, image)
+        # f = os.path.join(source, image)
+        print(f)
+        try:
+            img = Image.open(f).convert('RGB')
+            file_dest = "{}/{}.jpeg".format(dest, image)
+            img.rotate(rotate).resize((x, y)).convert('RGB').save(file_dest)
+            print('success edited {} and move to {}'.format(f, file_dest))
+        except IOError as e:
+            print("Can't edit {} because {}".format(f, e))
+
+if __name__ == '__main__':
+    source = '/home/irfancr/images'
+    dest = '/home/irfancr/Practices/Bangkit/images'
+    edit_image(source, dest)
+
+        
