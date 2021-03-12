728b16bd8eec021f34a7988cd61508e2d9585982
Irfan Chairurrachman <irfanchairurrachman@gmail.com>
2021-03-12 21:16:03 +0700

    fix: solve run.py

--- 6_AutomatingRealTask/run.py
+++ 6_AutomatingRealTask/run.py
@@ -1,6 +1,8 @@
 import os
+import requests
 
-def generate_text(dir):
+def generate_post(dir, url):
+
     feedbacks = []
     
     for txt in os.listdir(dir):
@@ -15,8 +17,15 @@ def generate_text(dir):
                 'feedback': lines[3].replace('\n', '')
             })
 
-    return feedbacks
+    for feedback in feedbacks:
+        response = requests.post(url, json=feedback)
+
+        if not response.ok:
+            raise Exception("FAILED to POST, reponse: {}, text title: {}".format(response.status_code, feedback['title']))
+        print("Feedback title: {} added!, response: {}".format(feedback['title'], response.status_code))
+
 
 if __name__ == '__main__':
     dir = '/home/irfancr/Practices/Bangkit/autopy/6_AutomatingRealTask/text'
-    print(generate_text(dir))
+    url = 'http://<url>/feedback/'
+    print(generate_post(dir, url))
