c1784e0c9990ca85d2d5efaa323e05ee0feb8549
Irfan Chairurrachman <irfanchairurrachman@gmail.com>
2021-03-14 13:17:05 +0700

    feat: add supplier_image.py

---
+++ 6_AutomatingRealTask/automate_catalog/supplier_image_upload.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import requests
+import os
+
+url = "http://localhost/upload/"
+path = 'supplier-data/images/'
+
+list_img = [os.path.join(image_path, img) for img in os.listdir(path) if '.jpeg' in img]
+
+for img in list_img:
+    with open(img, 'rb') as opened:
+        r = requests.post(url, files={'file': opened})
